921377b92dbd0bd6fb0e048e1293a59738ede9ce
Mukotso <kelvinmukotso@gmail.com>
2024-01-16 21:47:59 +0300

    remove unused files and images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Bootslander
-* Updated: Jan 09 2024 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
